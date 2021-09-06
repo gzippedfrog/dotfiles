e659fd4bf07d522b584216012f2c906818aa50cf
gzippedfrog <gzippedfrog@outlook.com>
2021-09-07 01:41:17 +0300

    some changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,34 +5,10 @@ else
     set EDITOR "vim" 
 end
 
-set TEST "a"
 set LESSHISTFILE -
-set TEST $TEST "b"
 set PATH $PATH ~/.local/bin
 
-## aliases
-
-## general
-alias v="$EDITOR"
-alias wttr="curl https://www.wttr.in"
-alias s="sudo"
-alias sv="sudo $EDITOR"
-alias stow="stow -t ~/ -v --ignore '\.DS_Store' --ignore '\.git'"
-alias la="ls -la"
-
-## pacman
-# alias p="sudo pacman"
-# alias upg="sudo pacman -Syu"
-
-## apt
-# alias ap="sudo apt"
-# alias upg="sudo apt update && sudo apt upgrade"
-
-## soystemd 
-# alias off="systemctl poweroff"
-# alias reb="systemctl reboot"
-# alias susp="systemctl suspend"
-# alias bios="systemctl reboot --firmware-setup"
+source ~/.config/aliases
 
 if status --is-interactive
     abbr --add --global stowdir ~/.config/stow
