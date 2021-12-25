bc4ad1e99d37bfd439ac9dd2422f87e217fba2f8
gzippedfrog <gzippedfrog@outlook.com>
2021-12-25 23:04:59 +0300

    minor changes

--- fish/.config/fish/config.fish
+++
@@ -1,3 +0,0 @@
-[ -x "(command -v nvim)" ] && export EDITOR=nvim || export EDITOR=vim
-[ -e ~/.local/bin ] && set PATH $PATH:(find ~/.local/bin -maxdepth 2 -type d | tr '\n' ':')
-[ -e ~/.config/aliases ] && source ~/.config/aliases
